d867ea72f934e66f996d7a8f167340b8b943d011
1BM18CS111 <53754240+1BM18CS111@users.noreply.github.com>
2019-08-22 15:55:18 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class student
+{
+  public static void main(String array[])
+ {
+  double sgpa = 0;
+  int n;
+  scanner obj= new scanner(system.in);
+  System.out.ptintln("enter  name,USN,number of objects");
+  string name = object;next();
+  string USN = object; next();
+  n = object next ()
+ System.out.println("enter credits and marks");
+ double marks= new marks
+ for(int i=0;i<n;i++)
+ {
+  System.out.println("subject "+ 1);
+  marks[i]= object next double()
+  credits[i]= object next double()
+  if (marks [i]>=90)
+   grade[i]=10;
+  else if (marks [i]>=75)
+   grade[i]=9;
+  else if (marks[i]>=60)
+   grade[i]=8;
+  else if (marks [i]>=50)
+   grade[i]=7;
+  else if(marks [i].=40)
+   grade[i]=6;
+  else if
+    grade[i]=0;
+   sgpa += credits[i]*grades[i];
+   total_credits+=credits[i];
+   }
+   sgpa/=total_credits;
+  System.out.println("SGPA="+sgpa);
+  
+  }
+  }
